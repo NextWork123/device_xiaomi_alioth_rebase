14c9053763121024e770c3ec9ab1c953cdddd321
NextWork123 <nextdesigns@libero.it>
2022-01-14 17:42:01 +0100

    Alioth: Bring-up Project Elixir

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -7,8 +7,8 @@
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-# Inherit some common AospExtended stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common  Project Elixir stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_alioth
@@ -16,18 +16,20 @@ PRODUCT_DEVICE := alioth
 PRODUCT_BRAND := POCO
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := POCO F3
-TARGET_SCREEN_DENSITY:=450
+TARGET_SCREEN_DENSITY:= 450
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# AEX
-TARGET_FACE_UNLOCK_SUPPORTED := true
-USE_PIXEL_CHARGER_IMAGES := true
-TARGET_SHIP_GCAM_GO := true
-TARGET_SUPPORTS_BLUR := true
-PRODUCT_BOARD_PLATFORM := kona
-PRODUCT_USES_QCOM_HARDWARE := true
-WITH_GMS := true
 WITH_GAPPS := true
+TARGET_INCLUDE_WIFI_EXT := true
+
+# Project Elixir Official Stuff
+Elixir_MAINTAINER := NextWorks 
+IS_PHONE := true
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+
